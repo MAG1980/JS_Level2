c8704459f481405c80856ff2da4c75b36533c906
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2021-10-17 12:07:06 +0300

    Заработало добавление товаров в корзину на сервере.

--- Lesson7/server/cartRouter.js
+++ Lesson7/server/cartRouter.js
@@ -2,21 +2,23 @@ const express = require('express');
 const fs = require('fs');
 const router = express.Router();
 const handler = require('./handler');
+const path = require("path");
 
 router.get('/', (req, res) => {
-    fs.readFile('server/db/userCart.json', 'utf-8', (err, data) => {
-        if(err){
-            res.sendStatus(404, JSON.stringify({result:0, text: err}));
-        } else {
-            res.send(data);
+    fs.readFile(path.resolve(__dirname, 'db/userCart.json'), 'utf-8', (err, data) => {
+            if (err) {
+                res.sendStatus(404, JSON.stringify({result: 0, text: err}));
+            } else {
+                res.send(data);
+            }
         }
-    })
+    )
 });
 router.post('/', (req, res) => {
-    handler(req, res, 'add', 'server/db/userCart.json');
+    handler(req, res, 'add', path.resolve(__dirname, 'db/userCart.json'));
 });
 router.put('/:id', (req, res) => {
-    handler(req, res, 'change', 'server/db/userCart.json');
+    handler(req, res, 'change', path.resolve(__dirname, 'db/userCart.json'));
 });
 
 module.exports = router;
